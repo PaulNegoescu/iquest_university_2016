b531fe15b54893c1eda96e7191428dad52b84d64
Wili Grosz <groszwilhelm@gmail.com>
2016-07-06 16:50:53 +0300

    project structure

---
+++ structure/src/app/app.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('structure')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
