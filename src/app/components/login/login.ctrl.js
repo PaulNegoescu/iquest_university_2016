74a07cb93ecac0e674e55add1bf28fa4577f54b0
Lacatus Florentina <lacatus.florentina@yahoo.com>
2016-07-08 15:04:49 +0300

    Updated login ctrl

--- src/app/components/login/login.ctrl.js
+++ src/app/components/login/login.ctrl.js
@@ -5,7 +5,6 @@
 
     function LoginController(Session, $state){
         var self = this;
-        console.log("kdsjhfskjhf");
         this.login = function(){
             Session.login(self.identifier, self.password).then(function(response){
                 if(response['token']){
