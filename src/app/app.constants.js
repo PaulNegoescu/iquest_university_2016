4388d327895ef9d57256212c8cd88fc54b35c008
Wili Grosz <groszwilhelm@gmail.com>
2016-07-08 11:05:46 +0300

    removed toastr

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -1,10 +1,4 @@
-/* global malarkey:false, moment:false */
 (function() {
   'use strict';
-
-  angular
-    .module('marathon')
-    .constant('malarkey', malarkey)
-    .constant('moment', moment);
 
 })();
