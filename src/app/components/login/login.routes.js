3380427392bf1ad21efb5505028b7278ef4483db
Lacatus Florentina <lacatus.florentina@yahoo.com>
2016-07-08 12:45:06 +0300

    Modified login routes

--- src/app/components/login/login.routes.js
+++ src/app/components/login/login.routes.js
@@ -1,5 +1,11 @@
 (function(){
     'use strict';
 
-    angular.module('marathon')
+    angular.module('marathon').config(function($stateProvider) {
+        $stateProvider.state('login', {
+            url:'/login',
+            templateUrl: 'app/components/login/login.view.html',
+            controller:'LoginController as vm'
+        });
+    });
 })();
