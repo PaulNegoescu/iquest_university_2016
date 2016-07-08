a98d23064b4374cc832823263fe78eade37b85c6
Lacatus Florentina <lacatus.florentina@yahoo.com>
2016-07-08 13:17:55 +0300

    Removed duplicate module declaration

--- src/app/components/login/login.ctrl.js
+++ src/app/components/login/login.ctrl.js
@@ -18,10 +18,4 @@
 })();
 
 
-//cand fac click pe login, se face submit la formular (fac ng-submit pe form)
-//trebuie verificat daca au fost completate campurile, si daca nu, ar trebui sa afisez ng-show un mesaj
-//daca autentificarea s-a facut cu succes, user-ul este redirectionat catre homepage (/dash) -> trebuie sa stabilesc ruta
-// (dar nu stiu unde...in app.route???)
-//cum validam daca au fost completate ambele campuri inainte de a face request?
-//unde facem validarile in functie de raspunsul pe care il primim de la server?
 
