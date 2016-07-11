0b7728296078deba72def2bf7b5ce43befa08c5c
Wili Grosz <groszwilhelm@gmail.com>
2016-07-11 16:12:48 +0300

    added register route in users.routes.js

--- src/app/components/users/users.routes.js
+++ src/app/components/users/users.routes.js
@@ -2,6 +2,10 @@
     'use strict';
 
     angular.module('marathon').config(function($stateProvider) {
-
+         $stateProvider.state('register', {
+            url:'/register',
+            templateUrl: 'app/components/users/users_register.view.html',
+            controller:'RegisterController as vm'
+        });
     });
 })();
