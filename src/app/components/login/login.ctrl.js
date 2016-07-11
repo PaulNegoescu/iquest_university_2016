e75896d9e1b2b5cece9b265af64769c4fb34f648
Lacatus Florentina <lacatus.florentina@yahoo.com>
2016-07-11 17:24:50 +0300

    Refactor Login with Formly

--- src/app/components/login/login.ctrl.js
+++ src/app/components/login/login.ctrl.js
@@ -5,15 +5,50 @@
 
     function LoginController(Session, $state){
         var self = this;
+<<<<<<< Updated upstream
+=======
+        self.user = {};
+
+>>>>>>> Stashed changes
         this.login = function(){
             Session.login(self.identifier, self.password).then(function(response){
-                if(response['token']){
+                if(response.status == 200){
                     $state.go('dash');
                 }
             });
         };
-    }
 
+        this.register = function(){
+            $state.go('register');
+        };
+
+        self.userFields = [
+            {
+                key: 'username',
+                type: 'input',
+                templateOptions: {
+                    type: 'text',
+                    label: 'User Name',
+                    required: true,
+                    minlength: 1,
+                    maxlength: 100,
+                    placeholder: 'User Name'
+                }
+            },
+            {
+                key: 'password',
+                type: 'input',
+                templateOptions: {
+                    type: 'password',
+                    label: 'Password',
+                    required: true,
+                    minlength: 5,
+                    maxlength: 100,
+                    placeholder: 'Password must be at least 5 characters long'
+                }
+            }
+        ]
+    }
 })();
 
 
