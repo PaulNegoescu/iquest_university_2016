214cda9e4f38cd249b608fd13c2a0bde91845e7a
Wili Grosz <groszwilhelm@gmail.com>
2016-07-13 12:40:03 +0300

    added user role at login

--- src/app/components/login/login.ctrl.js
+++ src/app/components/login/login.ctrl.js
@@ -3,12 +3,13 @@
 
     angular.module('marathon').controller('LoginController', LoginController);
 
-    function LoginController(Session, $state){
+    function LoginController(Session, $state, crAcl){
         var self = this;
 
         this.login = function(){
             Session.login(self.identifier, self.password).then(function(response){
                 if(response.status == 200){
+                    crAcl.setRole("ROLE_USER");
                     $state.go('dash');
                 }
             });
