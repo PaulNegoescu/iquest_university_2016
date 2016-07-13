75bdb4902edb813e3ba6c1d8e27e86be71016a95
Wili Grosz <groszwilhelm@gmail.com>
2016-07-13 11:09:19 +0300

    updated app module

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,6 +1,5 @@
 (function() {
   'use strict';
-  angular
-    .module('marathon', ['ui.router', 'ui.bootstrap', 'apiMock']);
+  angular.module('marathon', ['ui.router', 'ui.bootstrap', 'apiMock','formly','formlyBootstrap','LocalStorageModule','ngMessages','cr.acl']);
 
 })();
