b072e179691709d8707049dbe92d037059b7398c
Lacatus Florentina <lacatus.florentina@yahoo.com>
2016-07-13 11:59:51 +0300

    Resolved merging problems

--- src/app/components/users/users.service.js
+++ src/app/components/users/users.service.js
@@ -8,32 +8,6 @@
         var model = Object.create(BaseModel);
         model.entity = 'users';
 
-        model.loginFields = [
-            {
-                key: 'username',
-                type: 'input',
-                templateOptions: {
-                    type: 'text',
-                    label: 'User Name',
-                    required: true,
-                    minlength: 1,
-                    maxlength: 100,
-                    placeholder: 'User Name'
-                }
-            },
-            {
-                key: 'password',
-                type: 'input',
-                templateOptions: {
-                    type: 'password',
-                    label: 'Password',
-                    required: true,
-                    minlength: 5,
-                    maxlength: 100,
-                    placeholder: 'Password must be at least 5 characters long'
-                }
-            }
-        ];
         return model;
     }
 })();
