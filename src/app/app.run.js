aadb303c50476b5937ff544dc5e9e8cfcfb61d6f
Wili Grosz <groszwilhelm@gmail.com>
2016-07-13 11:11:34 +0300

    update on app run with roles

--- src/app/app.run.js
+++ src/app/app.run.js
@@ -1,14 +1,27 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('marathon')
-    .run(runBlock);
+    angular
+        .module('marathon')
+        .run(runBlock);
 
-  /** @ngInject */
-  function runBlock($log) {
+    /** @ngInject */
+    function run(crAcl) {
 
-    $log.debug('runBlock end');
-  }
+    }
+
+    function runBlock($log, $state, crAcl) {
+        console.log($state.get());
+        $log.debug('runBlock end');
+        crAcl.setInheritanceRoles({
+            /*"ROLE_ADMIN" : ["ROLE_ADMIN"],
+            "ROLE_CUSTOMER" : ["ROLE_ADMIN","ROLE_USER"],
+            "ROLE_CLIENT" : ["ROLE_ADMIN","ROLE_CUSTOMER","ROLE_USER"]*/
+            "ROLE_GUEST" : ["ROLE_GUEST"],
+            "ROLE_USER" : ["ROLE_USER"],
+            "ROLE_ADMIN" : ["ROLE_ADMIN"]
+        });
+        crAcl.setRedirect('login');
+    }
 
 })();
