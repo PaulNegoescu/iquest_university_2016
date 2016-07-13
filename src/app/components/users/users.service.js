80e54bef80f9743bdfe6fe6e3729e177e7cca21b
Lacatus Florentina <lacatus.florentina@yahoo.com>
2016-07-13 09:25:10 +0300

    updated manageUsers view, ctrl, service

--- src/app/components/users/users.service.js
+++ src/app/components/users/users.service.js
@@ -1,13 +1,39 @@
 (function() {
     'use strict';
 
-    angular.module('marathon').factory('Users', Users)
+    angular.module('marathon').factory('Users', Users);
 
     function Users(BaseModel) {
 
         var model = Object.create(BaseModel);
         model.entity = 'users';
 
+        model.loginFields = [
+            {
+                key: 'username',
+                type: 'input',
+                templateOptions: {
+                    type: 'text',
+                    label: 'User Name',
+                    required: true,
+                    minlength: 1,
+                    maxlength: 100,
+                    placeholder: 'User Name'
+                }
+            },
+            {
+                key: 'password',
+                type: 'input',
+                templateOptions: {
+                    type: 'password',
+                    label: 'Password',
+                    required: true,
+                    minlength: 5,
+                    maxlength: 100,
+                    placeholder: 'Password must be at least 5 characters long'
+                }
+            }
+        ];
         return model;
     }
 })();
