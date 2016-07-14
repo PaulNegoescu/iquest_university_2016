ff8d3f38882b5a7b09f799d791704c084d7ad5cd
Alexandra Teleanu <alexandra.teleanu94@gmail.com>
2016-07-14 16:42:40 +0300

    Modified User List - added user roles

--- src/app/components/users/users_list.ctrl.js
+++ src/app/components/users/users_list.ctrl.js
@@ -3,43 +3,60 @@
 
     angular.module('marathon').controller('usersListController', usersListCtrl);
 
-    function usersListCtrl(Users, $uibModal, $log){
+    function usersListCtrl(ConfirmationModal, Users, $log) {
         var self = this;
 
         this.searchUser = '';
 
-        Users.read().then(function(result){
+        this.shouldShowButtonPfm = true;
+
+        this.shouldShowButtonAdmin = true;
+
+        Users.read().then(function(result) {
             self.users = result;
         });
 
-        this.openConfirmationModal = function(user, index){
-            var modalInstance = $uibModal.open({
-                templateUrl: 'app/common/confirm_modal/confirm_modal.view.html',
-                controller: 'confirmModalController as vm',
-                size: 'md',
-                resolve: {
-                    message: function () {
-                        return 'Are you sure you want to delete user ' + user.firstname + ' ' + user.lastname + '?';
-                    },
-                    selectedItem: function(){
-                        return index;
-                    }
+        function openConfirmation(message, index, cb) {
+            ConfirmationModal.openModal(message, index).result.then(function (selectedItem) {
+                if(typeof cb === 'function') {
+                    cb(selectedItem);
                 }
-            });
-
-            modalInstance.result.then(function (selectedItem) {
-                removeUser(selectedItem);
                 $log.info('Modal dismissed with OK at:' + new Date());
             }, function () {
                 $log.info('Modal dismissed with Cancel at: ' + new Date());
             });
         };
 
-        function removeUser(selectedUser){
+        this.openDeleteConfirmation = function(user, index) {
+            openConfirmation('Are you sure you want to delete user ' + user.firstname + ' ' + user.lastname + '?', index, removeUser);
+        };
+
+        this.openPfmConfirmation = function(user, index, action) {
+            this.shouldShowButtonPfm = false;
+            openConfirmation('Are you sure you want to ' + action + ' ' + user.firstname + ' ' + user.lastname + ' as PFM?', index, function(selectedUser) { toggleRole(selectedUser, 'pfm'); });
+        };
+
+        this.openAdminConfirmation = function(user, index, action) {
+            this.shouldShowButtonAdmin = false;
+            openConfirmation('Are you sure you want to ' + action + ' ' + user.firstname + ' ' + user.lastname + ' as ADMIN?', index, function(selectedUser) { toggleRole(selectedUser, 'admin'); });
+        };
+
+        function toggleRole(selectedUser, role) {
+            var index = self.users[selectedUser].roles.indexOf(role);
+            if (index >= 0) {
+                self.users[selectedUser].roles.splice(index, 1);
+                Users.update(self.users[selectedUser]);
+            } else {
+                self.users[selectedUser].roles.push(role);
+                Users.update(self.users[selectedUser]);
+            }
+        }
+
+        function removeUser(selectedUser) {
             self.users.splice(selectedUser, 1);
         }
 
-        this.search = function(row){
+        this.search = function(row) {
             var excludeFromSearch = ['id', '$$hashKey'];
             var result = false;
             for (var key in row) {
@@ -48,8 +65,7 @@
                 }
             }
             return result;
-        }
+        };
     }
-
-
 })();
+
