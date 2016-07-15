f4f4eafd68ea1042d8ddbb1fbd503380219b676b
Alexandra Teleanu <alexandra.teleanu94@gmail.com>
2016-07-15 16:00:05 +0300

    Created Roles Service

---
+++ src/app/components/roles/roles.service.js
@@ -0,0 +1,13 @@
+(function(){
+    'use strict';
+
+    angular.module('marathon').service('RolesService', RolesService);
+
+    function RolesService(apiService) {
+    	this.entity = 'roles';
+
+    	this.read = function() {
+            return apiService.read(this.entity);
+        };
+    }
+})();
